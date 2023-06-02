84cef3241142822965066e8fbdfe00259ab9f472
Alejandro Armas <lxarmas@gmail.com>
2023-06-01 18:01:30 -0700

    CORS initiliaze

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,44 @@
-from fastapi import FastAPI
+
+# uvicorn main:app
+
+# uvicorn main:app --reload
+
+
+# Main Imports
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
+from decouple import config
+import openai
+
+# Custom Function Import
+# ...
+
+# Initiate App
 app = FastAPI()
 
-@app.get("/")
-async def root():
+# CORS - Origins
+origins = [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "http://localhost:4173",
+    "http://localhost:4174",
+    "http://localhost:3000",
+]
 
-    return {"message":"Hello World"}
+# CORS - MiddleWare
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Check Health
+
+
+@app.get("/health")
+async def check_health():
+    print("hello")
+    return {"message": "health"}
